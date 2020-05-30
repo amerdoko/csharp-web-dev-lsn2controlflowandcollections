bbe669f4b518cecf7e688dd7d77cc59e7ec84a48
Amer Doko <amerdoko@gmail.com>
2020-05-30 13:19:06 -0400

    Added nums array and printed its members.

--- ArrayExercises/Program.cs
+++ ArrayExercises/Program.cs
@@ -7,6 +7,11 @@ namespace ArrayExercises
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Arrays!");
+            int[] nums = { 1, 1, 2, 3, 5, 8 };
+            foreach (int num in nums)
+            {
+                Console.WriteLine(num);
+            }
         }
     }
 }
